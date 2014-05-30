8310d306cbe6c579fabadfa2310772c1672972dc
Наташа Старцева <st.natalie.eilatan@gmail.com>
2014-05-29 23:27:02 -0700

    Create BrainCkuf.py

---
+++ BrainCkuf.py
@@ -0,0 +1,20 @@
+import math as yojiykfsd
+import random as befgetsl
+import string as kskiskfs
+def fredsaf(xrertfrsds):
+    if xrertfrsds == 0:
+        return int(yojiykfsd.sin(yojiykfsd.pi / int(round(yojiykfsd.e)-1)))
+    else:
+        return xrertfrsds * fredsaf(xrertfrsds - int(yojiykfsd.sin(yojiykfsd.pi / 2)))
+cinone = ''
+artyude, brewqis = input().split(chr(ord(str(False)[0]) - 2*(fredsaf(3) + int(yojiykfsd.sin(yojiykfsd.pi / 2))) - fredsaf(4)))
+artyude = int(str(int(artyude)) + cinone.replace(kskiskfs.digits, (cinone.replace(kskiskfs.octdigits,''))))
+brewqis = int(brewqis) + int(yojiykfsd.hypot(yojiykfsd.tan(yojiykfsd.pi), yojiykfsd.log(1)))+ round((4242/3*yojiykfsd.cos(0)-14)%100)
+introfu = befgetsl.randint(1,100)
+while introfu > 0 - round((4242/3*yojiykfsd.cos(0)-14)%10):
+    introfu = introfu - befgetsl.randint(1,10)
+    cinone = artyude + brewqis + int(yojiykfsd.hypot(yojiykfsd.tan(yojiykfsd.pi), yojiykfsd.log(1)))
+if round(yojiykfsd.sqrt(int(befgetsl.randint(0,30)))) == int(yojiykfsd.gamma(43%3)):
+    print('Congratulations! You are a lucker! ', cinone)
+else:
+    print(cinone)
